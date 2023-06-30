3842ac826436f36c98c21f4d0dd7d17888f822dc
jenn <jennifer.nguyen@ibm.com>
2023-06-30 14:30:43 -0700

    init

---
+++ mvi/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DEPLOY_DIR="/home/itzuser/ieam-edge-mvi-demo/mvi"
+. ~/env.sh
+
+APP_IMAGE_BASE=na.artifactory.swg-devops.com/igm-ibm-gsc-team-docker-local/marvinimages/cluster:mvi-edge
+IMAGE_VERSION=latest
+APP_IMAGE=$APP_IMAGE_BASE:$IMAGE_VERSION
+OPERATOR_IMAGE_BASE="docker.io/appimage/ieam-edge-mvi-operator"
+OPERATOR_IMAGE=$OPERATOR_IMAGE_BASE:$IMAGE_VERSION
+
+cd $DEPLOY_DIR && git stash && git pull
+cd config/manager && kustomize edit set image controller="$OPERATOR_IMAGE" && cd ../..
+sed -i -e "s|{{APP_IMAGE_BASE}}|$APP_IMAGE_BASE|" config/samples/demo.yaml
+sed -i -e "s|{{IMAGE_VERSION}}|$IMAGE_VERSION|" config/samples/demo.yaml
+
+# Update Version in horizon/hzn.json if you make ANY change
+mv horizon/hzn.json /tmp/hzn.json
+jq --arg IMAGE_VERSION "$IMAGE_VERSION" '.MetadataVars.SERVICE_VERSION |= $IMAGE_VERSION' /tmp/hzn.json > horizon/hzn.json
+
+make docker-build docker-push IMG=$OPERATOR_IMAGE
+
+rm operator.tar.gz & rm -rf deploy && mkdir -p deploy
+kustomize build config/default > deploy/kustomize_manifests_operator.yaml
+tar -C deploy -czf operator.tar.gz . && rm -rf deploy
+
+hzn exchange service publish -f $DEPLOY_DIR/horizon/service.definition.json --overwrite
+HZN_POLICY_NAME="albertsonsce/policy-cluster-mvi-app"
+hzn exchange deployment removepolicy -f $HZN_POLICY_NAME
+sleep 10
+hzn exchange deployment addpolicy -f $DEPLOY_DIR/horizon/service.policy.json $HZN_POLICY_NAME
+# hzn exchange deployment updatepolicy -f $DEPLOY_DIR/horizon/service.policy.json $HZN_POLICY_NAME
