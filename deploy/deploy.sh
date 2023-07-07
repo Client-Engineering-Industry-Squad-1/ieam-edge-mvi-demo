4cc132289c98996d04560d1665295630eecc6df4
jenn <jennifer.nguyen@ibm.com>
2023-07-07 09:24:46 -0700

    Update service and policy name

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -25,7 +25,7 @@ kustomize build config/default > deploy/kustomize_manifests_operator.yaml
 tar -C deploy -czf operator.tar.gz . && rm -rf deploy
 
 hzn exchange service publish -f $DEPLOY_DIR/horizon/service.definition.json --overwrite
-HZN_POLICY_NAME="albertsonsce/policy-cluster-mvi-app"
+HZN_POLICY_NAME="albertsonsce/policy-cluster-ai-app"
 hzn exchange deployment removepolicy -f $HZN_POLICY_NAME
 sleep 10
 hzn exchange deployment addpolicy -f $DEPLOY_DIR/horizon/service.policy.json $HZN_POLICY_NAME
